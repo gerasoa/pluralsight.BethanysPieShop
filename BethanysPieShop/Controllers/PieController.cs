02c4ca49658dad3599f4d84697cba847227709af
Rogerio Alves Soares <gerasoa@gmail.comgit config --global user.name Rogerio>
2020-07-28 16:33:16 +0100

    First version of site finished

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this._pieRepository = pieRepository;
+            this._categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+
+            pieListViewModel.CurrentCategory = "Cheese cakes";
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            
+            return View(pieListViewModel);
+        }
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,10 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +31,17 @@ namespace BethanysPieShop
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
